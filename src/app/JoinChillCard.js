7cefaa7843f62bb34bbeadcb54c72316652b800e
simonjpartridge <me@simonjpartridge.co.uk>
2016-05-04 20:52:06 +0100

    added first chill

--- src/app/JoinChillCard.js
+++ src/app/JoinChillCard.js
@@ -6,13 +6,14 @@ import CardMedia from 'material-ui/Card/CardMedia';
 import CardTitle from 'material-ui/Card/CardTitle';
 import FlatButton from 'material-ui/FlatButton';
 import CardText from 'material-ui/Card/CardText';
+import ChillOverview from './ChillOverview';
 
 class JoinChillCard extends React.Component {
 
 
 
   render (){
-
+    var tempChill= {"tags": ["code"], "location": "SimpleWeb", "user": "Ben"}
         // <CardMedia
         //   overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
         // >
@@ -22,7 +23,7 @@ class JoinChillCard extends React.Component {
         // </CardActions>
 
     return (
-      <Card>
+      <Card className={this.props.className}>
         <CardHeader
           title="Join a Chill"
           subtitle="Chill &..."
@@ -30,7 +31,7 @@ class JoinChillCard extends React.Component {
         />
         <CardTitle title="Card title" subtitle="Card subtitle" />
         <CardText>
-          Start a chill somewhere
+          <ChillOverview info={tempChill}/>
         </CardText>
 
       </Card>
