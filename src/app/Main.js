66172ae12b72d5228d2881a5913542fc07502fd5
simonjpartridge <me@simonjpartridge.co.uk>
2016-05-04 20:15:17 +0100

    started adding components

--- src/app/Main.js
+++ src/app/Main.js
@@ -10,6 +10,7 @@ import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
 const styles = {
   container: {
@@ -32,6 +33,7 @@ class Main extends React.Component {
 
     this.state = {
       open: false,
+      title: "Main"
     };
   }
 
@@ -59,20 +61,9 @@ class Main extends React.Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>material-ui</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            primary={true}
-            onTouchTap={this.handleTouchTap}
+          <AppBar
+            title={this.state.title}
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
           />
         </div>
       </MuiThemeProvider>
