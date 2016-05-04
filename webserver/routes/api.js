36038b339f042f62fed9d46e5f1c2988b15ec522
Rory Smith <roarsmith3@gmail.com>
2016-05-04 20:36:57 +0100

    typo:

--- webserver/routes/api.js
+++ webserver/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/nearby', function(req, res, next) {
-  res.json([{"tags": ["code"], "location": "SimpleWeb", "user": "Ben"}, {"tags": ["eat"], "location": "SimpleWeb"}, "user": "Simon"]);
+  res.json([{"tags": ["code"], "location": "SimpleWeb", "user": "Ben"}, {"tags": ["eat"], "location": "SimpleWeb", "user": "Simon"}]);
 });
 
 module.exports = router;
